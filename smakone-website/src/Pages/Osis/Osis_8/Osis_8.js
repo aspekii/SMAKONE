75c0d45124d2feae416c00dfe4f07ba024e645ab
aspekii <154879347+aspekii@users.noreply.github.com>
2026-02-18 23:23:51 +0700

    BID 8 & 9

--- smakone-website/src/Pages/Osis/Osis_8/Osis_8.js
+++ smakone-website/src/Pages/Osis/Osis_8/Osis_8.js
@@ -5,9 +5,11 @@ import BidangSection from '../../../Component/Osis/BidangSection'
 import OsisTeam from '../../../Component/Osis/OsisTeam'
 
 // Import Image
-import Laurensia from '../../../Image/Osis/Bidang_8/Laurensia.webp'
-import Grace from '../../../Image/Osis/Bidang_8/Grace.webp'
-import Yovela from '../../../Image/Osis/Bidang_8/Yovela.webp'
+import Laurensia from '../../../Image/Osis/Bidang_8/Laurensia.png'
+import Cleon from '../../../Image/Osis/Bidang_8/Cleon.jpg'
+import Gabriel from '../../../Image/Osis/Bidang_8/Gabriel.jpg'
+import Nadya from '../../../Image/Osis/Bidang_8/Nadya.jpg'
+import Jocelyn from '../../../Image/Osis/Bidang_8/Jocelyn.jpg'
 
 
 const OSIS_8 = () => {
@@ -16,12 +18,12 @@ const OSIS_8 = () => {
       <OsisTeam
         bidang="Bidang 8"
         desc="Bidang Pembinaan Sastra dan Budaya"
-    
-        person1={<BidangSection img={Yovela} alt='Yovela' name='Yovela Dania Hewis' desc='Haiii semuanya!! salken nama aku Yovela Dania Hewis, biasanya dipanggil yovela / pei pei, ato sayang juga boleh.. HAHAHHAHA. Aku suka nari 🕺, baking 👩‍🍳🍪, sama nonton film 🎬🍿. selain menjadi koordinator bidang 8 aku juga tergabung di F1RST, BBPB, REVER1E, dan OneJournal (writer). Jadi kalo F1RST tampil klian wajib banget rusuh!! Kalo mau ngobrol2 & mutualan juga boleh banget dm ig aku @yovelahewis. Have fun di smukiee!!'/>}
-        person2={<BidangSection img={Grace} alt='Grace' name='Grace Anindya Septiawan' desc='Hai semuaa! Nama aku Grace Anindya Septiawan, biasa dipanggil grace atau anin juga bolehh. Aku 24/7 on spotify, hobi main musik terutama gitar, and i love chamomile tea. Aku gak suka film horror yang hantunya seremm, tapi aku suka banget film thriller hehe =D. Selain OSIS aku juga gabung di club MASK btw. Kalo mau ngobrol2 bolehh banget dm aku di @as.grc_ yaa. Goodluck di SMUKIE semuanyaa!!'/>}
-        person3={<BidangSection img={Laurensia} alt='Laurensia' name='Laurensia Edeline Santosa' desc="Hai haii!!!👋🏻 Nama aku Laurensia Edeline Santosa, biasa dipanggil Laurenn. Aku dari kelas X1 dan bagian dari osis bidang 8✨. Aku suka banget main badminton 🏸 dan baca novell📰, terutama karya-karyanya Tere Liye!!! Selain itu, aku juga suka banget dengerin musik terutama genre R&B dan Pop🎷. Fun fact, aku phobia tinggi dan paling gabisa nonton film hororr 😢😢.  Kalau mau kasih rekomendasi novel seru atau tanya-tanya soal smukie boleh dm ig akuu @laurensiaedeline14. Thank youu<3"/>}
-        
-        
+
+        person1={<BidangSection img={Laurensia} alt='Laurensia' name='Laurensia Edeline Santosa' desc='Hai haii!!!👋🏻 Nama aku Laurensia Edeline Santosa, biasa dipanggil Lauren, Lau atau Renn. I’m the coordinator of Bid Eight, and yeahh… we ateee👌🏻 Hobiku bernyanyi 🎙️ dan membaca novel📰, terutama karya-karyanya Tere Liye!!! Selain itu, aku juga suka banget dengerin musik genre R&B dan Pop🎷. Fun fact, aku phobia tinggi, paling nggak bisa nonton film horor dan penggemar Nailong garis kerasss💛!!! Kalau mau kasih rekomendasi novel seru atau tanya-tanya soal Smukie, boleh DM IG akuu @laurensiaedeline ^^' />}
+        person2={<BidangSection img={Cleon} alt='Cleon' name='Cleon Wyn Setiawan' desc='kenalin nama aku Cleon Wyn Setiawan bisa dipanggil cleon. I’m part of Bid Eight, and yeah… we ateee👌🏻 Hobi aku adalah dance hip hop dan juga main piano, makanan favorit aku yang manis manis 😎. Kalau kalian mau tanya tanya tentang sesuatu, boleh chat ig aku ajaa di @wyncleon , salken semuaa‼️' />}
+        person3={<BidangSection img={Gabriel} alt='Gabriel' name='Gabriel Ailenka Cayadi ' desc="Halo semuaa 👋🏻👋🏻 Kenalin nama aku Gabriel Ailenka Cayadi bisa dipanggil Ailenka. I’m part of Bid Eight, and yeah… we ateee👌🏻Selain pengurus OSIS, aku juga join Serenity lohh 💃 Funfact, aku dari kecil suka banget nari, mulai dari tari tradisional Indonesia sampe modern, pokoknya suka smuanya dehh hehee.. Selain nari, aku juga suka cooking 🥘 baking 🍪 dan nonton 🎥 (btw kalo mau recommend series bole banget yaa!!). Anyways, if you want to know more about me, feel free to ask me on IG @gabrielailenka_ nice to meet you all 💗💗" />}
+        person4={<BidangSection img={Nadya} alt='Nadya' name='Nadya Cherry Hendro' desc='haii haii semuaa 👋🏻👋🏻👋🏻 kenalinn akuu 🍒 hehe iya cherry, panggil aja ceri, ceroy, apapun dehh asal jangan sayang, soalnya kalau udah baper ribet 😜, and i’m from bid Eight and yeah… we ateee👌🏻. this yearr aku dapet kesempatann as an anggota bidang 8 and i’m beyond grateful for thatt, sedikit tentang akuu sii aku suka dengerin dan main musikk especially violinn 🎻 and piano 🎹 dan aku hobi nontonn jugaa, that’s whyy i decided to join bidang 8, kalau kalian ada recommendation film or drama pleaseeee feel free to reach out to my ig (ceerriy) 🙂‍↕️, aku ga gigit kok ✌🏻andd i wishh kita bisa have fun together in smukiee 👋🏻gudluck in smukie and may the force be with you ✨' />}
+        person5={<BidangSection img={Jocelyn} alt='Jocelyn' name='Jocelyn Haosana' desc='haii all👋🏻!! Kenalin, nama aku Jocelyn Haosana biasanya dipanggil Jocee. I’m part of Bid Eight, and yeah… we ateee👌🏻Selain Osis, aku juga terlibat aktif dalam berbagai kepanitiaan di smukiee. Aku sukaaa banget nonton moviess (esp fantasy💭) andd fun fact, publication📱has also become one of my biggest interest. Soo kalo kalian mau nanya nanya 👀 seputar publii atau rec any kind of movies 🎞️ feel free to reach me out on my insta @jocelynnn.h. Lastlyy, wishing 🫵🏻 a great experience at smukiee‼️' />}
 
         Description={true}
         DescBid="Selamat datang di Bidang 8 yaitu Bidang Pembinaan Sastra dan Budaya. Bidang ini bertanggung jawab mengenai kegiatan non-akademik di bidang kesenian, sastra, dan budaya. Bidang 8 juga mendukung siswa-siswi SMAK 1 dalam menyalurkan talenta kesenian pada acara STN ( Smukiez Theatre Night), kemampuan memproduksi short movie pada SMURF, dan bidang jurnalistik pada program PENS1L. Selain itu, bidang 8 juga memantau dan mempersiapkan regenerasi club-club di SMAK 1 serta masih banyak program bidang 8 yang menarik seperti Birthday Calendar, CNYxVals, dan RESONANSI."
@@ -34,7 +36,8 @@ const OSIS_8 = () => {
             'CNYxVALS - perayaan harmoni budaya antara hari raya tahun baru Imlek dan Hari Kasih Sayang dengan menghadirkan acara yang menarik dengan nuansa imlek sekaligus romantis Valentines',
             'Smukiez Art Performance (SAP) - Menyelenggarakan pertunjukan seni yang mendalam dan beragam, mengapresiasi seni teater, vokal, musik, tari, dan lukis, membawa penonton dalam perjalanan artistik yang menggugah perasaan, serta memberikan penghormatan terhadap keindahan dan ekspresi kreatif dalam segala bentuk seni.',
             'Lagu Daerah - Pemutaran lagu daerah menggunakan radio SMAK 1 dengan tujuan membantu siswa-siswi dalam mengenali lagu daerah Indonesia',
-            "BEAT '8 - Memperkenalkan lagu-lagu Indonesia menggunakan radio sekolah, pada event tertentu seperti Valentine akan dibacakan pesan anonim yang dikirimkan melalui gforms di radio sekolah"
+            'Sen1man - Workshop seni untuk memberikan inspirasi baru bagi anggota clubs seni melalui pengalaman dan pelatihan yang dibagikan oleh pelatih luar sesuai keahlian bidangnya masing-masing.',
+            "Kalaswara - Kegiatan kunjungan  edukatif ke museum di Indonesia sebagai salah satu sarana pembelajaran seni bagi para siswa agar mereka tidak hanya dapat mempelajari seni namun juga menikmatinya."
           ]
         }
         dir="/OSIS/bidang9"
--- smakone-website/src/Pages/Osis/Osis_9/Osis_9.js
+++ smakone-website/src/Pages/Osis/Osis_9/Osis_9.js
@@ -5,10 +5,11 @@ import BidangSection from "../../../Component/Osis/BidangSection";
 import OsisTeam from "../../../Component/Osis/OsisTeam";
 
 // Import Image
-import Grant from "../../../Image/Osis/Bidang_9/Grant.webp";
-import Justin from "../../../Image/Osis/Bidang_9/Justin.webp";
-import Gisella from "../../../Image/Osis/Bidang_9/Gisella.webp";
-import Shenny from "../../../Image/Osis/Bidang_9/Shenny.webp";
+import Joanne from "../../../Image/Osis/Bidang_9/Joanne.jpg"
+import Richelle from "../../../Image/Osis/Bidang_9/Richelle.jpg"
+import Diego from "../../../Image/Osis/Bidang_9/Die.jpg"
+import Fernysia from "../../../Image/Osis/Bidang_9/Fernysia.jpg"
+import Shenny from "../../../Image/Osis/Bidang_9/Shenny.jpg"
 
 const OSIS_9 = () => {
   return (
@@ -17,39 +18,49 @@ const OSIS_9 = () => {
         bidang="Bidang 9"
         desc="Bidang Pembinaan Teknologi Informasi dan Komunikasi (TIK)"
 
-        
+
         person1={
           <BidangSection
-            img={Justin}
-            alt="Justin"
-            name="Justin Revaldo Widjaja"
-            desc="hai semuanyaa👋!! kenalin nama aku justin. Saat ini, aku merupakan warga kelas XI-4🔛🔝 & diberi kepercayaan untuk menjabat sebagai koordinator OSIS bidang 9👏! Selain di osis, aku juga suka ikut kepanitiaan seperti SAP, TEDx dan yang lainnyaa. Kalo lagi ada waktu luang, aku suka programming, main voli, and nonton filmm ofc. anyway mungkin perkenalanny cukup disini ajaa, kalo masih ada pertanyaan u can find me on ig @revaldoojustin. Have funn di smukie semuanyaa 🎇🤩‼"
+            img={Joanne}
+            alt="Joanne"
+            name="Joanne Gisella Yusmita"
+            desc="Koneksi kentang, tapi acara harus tetap jalan.. Kenalin aku Gisel, koorbid dari bidang sembilann 🙌🏻 funfactt, aku suka aktif dalam organisasi n clubs di smukie seperti Monograph, Shutterone, CodeOne (yang bantu bikin this website ur seeing rn hihi) and kegiatan kepanitiaan lainnya 👾 My passion are in coding, color grading, graphic design and many more 🦆 Dont worry, smukie lengkap banget kok untuk kembangin hobby ato skill kalian (●’∇’)♪ Semangat berjuang di smukie ol, kalo mau mutualan/temenan/anythingg bisa ke ig akuu @gisellayusmita 👈🏻 Acara mulus bukan karena satu orang.. tapi satu tim, jadii check out anak mulmed lainnya ya! 🙌 janlupaa pantau kita-kita di balik layar, biar acara nggak bubar 💥"
           />
         }
         person2={
           <BidangSection
-            img={Grant}
-            alt="Grant"
-            name="Grant Favor Tanudjaja "
-            desc="Halo semua, kenalin aku Grant, salah satu anggota Bidang 9. Tahun ini aku duduk di kelas XI5. Hobi aku main golf, main game, main musik, dll. Kalau mau nanya apa-apa atau kenalan dm aja ke @grantfavr"
+            img={Shenny}
+            alt="Shenny"
+            name="Shenny Pannya Lowis"
+            desc='Laptop nge-lag, lampu padam, tapi MC udah berdiri di depan....Kenalin aku Shenny, anggota dari bidang sembilann yey ☺️ Hobi aku simple : menonton film dan dengerin musik. Tetapi kl  di smukiee, aku suka banget aktif dalam organisasi n clubs, seperti MASK, Shutterone, ONEFM, dll. Jujur aja smukiee ada banyak banget club n ekskur yang kalian bisa ikuti dan kembangin skill terpendam kalian lho..jadi ayolah gas ikutt club-club di smukie hehe siapa tau kita bisa kerja barengg!! 😎🔥 If you need anything (mau ngobrol/mutualan/kenalan/dll) bisa dm akuu di @shennylowis yaa 💁🏻 Kunci sukses acara itu dari tim yang SOLID, jadi jan lupaa check out mulmed-erz lainnyaa yaa!! 🙌🏻 anyways goodluck n have fun in SMUKIE semua, 加油‼️'
           />
         }
         person3={
           <BidangSection
-            img={Shenny}
-            alt="Shenny"
-            name="Shenny Pannya Lowis"
-            desc="heloo everyonee!! kenalin nama aku Shenny Pannya Lowis, biasa dipanggil Shenny. Tahun ini aku berkesempatan untuk menjabat sebagai anggota OSIS bidang 9 yey. Di waktu luang, aku suka banget nonton film dan juga dengerin musik. Aku juga punya hobii main valorant..jadi kl kalian main juga, ayo gass kita mabar🔥..anyways if you need anything atau mau ngobrol, bisaa cari aku atau dm ajaa di @shennylowis !!  Goodluck n have fun in SMUKIE semua，加油‼️"
+            img={Richelle}
+            alt="Richelle"
+            name="Richelle Liu"
+            desc="Slide belum di share, tapi MC udah nungguin… haiii all!!👋🏻 Kenalin aku Richelle dari bidang 9️⃣, kalian bisa panggil aku ricell / richelle 😆. Hobi aku yaa nonton, baking👩‍🍳, scroll tiktok. Kalau mau mutualan atauu kenalan bolee bgtt dm akuu di @ricell.liu"
           />
         }
         person4={
           <BidangSection
-            img={Gisella}
-            alt="Gisella"
-            name="Joanne Gisella Yusmita"
-            desc="helloo everyonee!! kenalin aku Joanne Gisella, tapi biasanya dipanggil Gisel. aku hobi gambar/design, coding, video editing, animating, banyak deh ehehe 🤏🏻 funfact, aku suka bgtt join apapun yang berhubungan sama design, jadi jangan shok kalo lengah dikit bisa ketemuan di desdivv. anyways feel free kalo mau ngobrol atau mutualan yaa, @gisellayusmita"
+            img={Fernysia}
+            alt="Fernysia"
+            name="Fernysia Edbert Samsie"
+            desc="Sistem error, jaringan bermasalah, tapi koordinasi tim tetap jalan… haii semuanyaa👋!! kenalin aku Fernysia dari bidang sembilan 9️⃣!! Aku dari kelas 10-✌️dan kalian bisa panggil aku ferny. Aku suka banget dengerin lagu 🎧 apalagi lagunya LANY 🎶 and suka nonton film/series juga 🎬, jadi kalau ada any recommendation atau mau ngobrol ngobrol 💌, feel free to dm me on ig @fernysia 🙌!! goodluckk n have fun di smukiee 🫵‼️"
           />
         }
+        person5={
+          <BidangSection
+            img={Diego}
+            alt="Diego"
+            name="Diego Leander Wilbert"
+            desc="Sinyal ngadat, LED black out, tapi pembicara udah di depan. Hai semuanya 👋 nama aku Diego Leander Wilbert, salah satu anggota bidang sembilan, saat ini aku merupakan warga kelas X-4. Kalian bisa panggil aku Di, Go or Die, atau go die juga gapapa angangang🤣🤣 you can call me anything you want! Selain di OSIS, aku juga suka ikut kegiataan kepanitian di sekolah maupun di gereja. Kalo ada waktu luang biasanya aku main voli 🏐, main mobile legend 🎮, and nonton film . Kalau mau nanya apa-apa atau kenalan langsung dm aja ke @diegooo.lw. Have fun jadi warga smukie semuanya 🤩🎇"
+
+          />
+        }
+
         Description={true}
         DescBid="Bidang 9, Bidang Pembinaan Teknologi Informasi dan Komunikasi (TIK) adalah bidang yang bertanggung jawab mengenai pemanfaatan TIK untuk melancarkan kegiatan-kegiatan ataupun acara di sekolah, seperti menjadi tim multimedia saat ada seminar. Selain itu, bidang 9 menggunakan TIK sebagai sumber informasi bagi para siswa agar dapat mengenal OSIS SMAK 1 lebih dalam lagi. Bidang 9 juga peduli akan talenta siswa-siswi SMAK 1. Salah satu bentuk kepeduliannya adalah dengan mewadahi siswa-siswi yang ingin mengembangkan talentanya di bidang desain dan perfilman dengan Monograph dan Shutterone."
         Program={true}
